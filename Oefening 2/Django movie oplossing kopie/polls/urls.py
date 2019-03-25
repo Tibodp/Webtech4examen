565d3068ca99172cdd638bec74a76e47270c95fb
Tibo Depovere <tibo.depovere@gmail.com>
2019-03-25 16:36:53 +0100

    Commit 2 oef 2

--- Oefening 2/Django movie oplossing kopie/polls/urls.py
+++ Oefening 2/Django movie oplossing kopie/polls/urls.py
@@ -5,8 +5,8 @@ app_name = 'polls'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search/actor', views.search_movie, name='search_movie'),
-    path('add/movie', views.add_movie, name='add_movie'),
-    path('add/actor', views.add_actor, name='add_actor'),
-    path('<str:movie_name>/', views.detail, name='detail'),
+    # path('search/actor', views.search_movie, name='search_movie'),
+    # path('add/movie', views.add_movie, name='add_movie'),
+    # path('add/actor', views.add_actor, name='add_actor'),
+    # path('<str:movie_name>/', views.detail, name='detail'),
 ]
--- Oefening 2/Django movie oplossing kopie/polls/views.py
+++ Oefening 2/Django movie oplossing kopie/polls/views.py
@@ -1,47 +1,63 @@
 from django.shortcuts import render
 
+from datetime import datetime
+
+
 # Create your views here.
 from django.http import HttpResponse
 from .models import Movie
 from .models import Actor
 import string
+import datetime
+# now = datetime.datetime.now()
+from django.utils import timezone
+date_time_str = '2019-03-29 08:15:27.243860'  
+date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
 
-
+now = timezone.now() - datetime.timedelta(days=30)
+now2 = date_time_obj - timezone.now()
 def index(request):
-    #return HttpResponse("Hello, here will be a list with movies")
-    movie_name = [m.movie_name for m in Movie.objects.all()]
+    return render (request, 'polls/index.html', { 'time': now2})
 
-    return render(request, 'polls/index.html', {'movie_name': movie_name})
 
-def detail(request, movie_name):
-    movie =  Movie.objects.filter(movie_name=movie_name).first()
-    actor_name = movie.actor_set.all()
 
-    return render(request, 'polls/detail.html', {'actor_name': actor_name, 'movie_name': movie_name})
 
-def search_form(request):
-    return render(request, 'polls/index.html', {})
 
-def search_movie(request):
-    if request.method == 'POST':
-        word = request.POST['search_term']
-        movie_list = Movie.objects.all()
-        result_movie = [a.movie_name for a in movie_list if a.search_movie(word)]
-        return detail(request,next(iter(result_movie), None))
+# def index(request): 
+#     #return HttpResponse("Hello, here will be a list with movies")
+#     movie_name = [m.movie_name for m in Movie.objects.all()]
 
-def add_movie(request):
-        if request.method == 'POST':
-                movie_name = request.POST['movie_name']
-                movie_genre = request.POST['movie_genre']
-                Movie.objects.create(movie_name=movie_name, movie_genre=movie_genre)
-                return index(request)
-        else:        
-                return render(request, 'polls/add_movie.html', {})
+#     return render(request, 'polls/index.html', {'movie_name': movie_name})
+
+# def detail(request, movie_name):
+#     movie =  Movie.objects.filter(movie_name=movie_name).first()
+#     actor_name = movie.actor_set.all()
+
+#     return render(request, 'polls/detail.html', {'actor_name': actor_name, 'movie_name': movie_name})
+
+# def search_form(request):
+#     return render(request, 'polls/index.html', {})
+
+# def search_movie(request):
+#     if request.method == 'POST':
+#         word = request.POST['search_term']
+#         movie_list = Movie.objects.all()
+#         result_movie = [a.movie_name for a in movie_list if a.search_movie(word)]
+#         return detail(request,next(iter(result_movie), None))
+
+# def add_movie(request):
+#         if request.method == 'POST':
+#                 movie_name = request.POST['movie_name']
+#                 movie_genre = request.POST['movie_genre']
+#                 Movie.objects.create(movie_name=movie_name, movie_genre=movie_genre)
+#                 return index(request)
+#         else:        
+#                 return render(request, 'polls/add_movie.html', {})
         
-def add_actor(request):
-        if request.method == 'POST':
-                movie_name = request.POST['movie_name']
-                actor_name = request.POST['actor_name']
-                movie =  Movie.objects.filter(movie_name=movie_name).first()
-                Actor.objects.create(actor_name=actor_name, actor_movie=movie)
-                return index(request)
+# def add_actor(request):
+#         if request.method == 'POST':
+#                 movie_name = request.POST['movie_name']
+#                 actor_name = request.POST['actor_name']
+#                 movie =  Movie.objects.filter(movie_name=movie_name).first()
+#                 Actor.objects.create(actor_name=actor_name, actor_movie=movie)
+#                 return index(request)
